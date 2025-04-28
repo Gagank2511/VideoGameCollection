8c89e95ab612e2b8645c1949920ae592053d617b
Kaur <gagandeepmalh@gmail.com>
2025-04-28 13:00:35 +0100

    Added list  functions

--- gameLibrary.java
+++ gameLibrary.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class gameLibrary {
+public class GameLibrary {
 
     private static List<AbstractGame> games = new ArrayList<>();
     public static void add(SinglePlayer sPgame1) {
@@ -10,6 +10,19 @@ public class gameLibrary {
 
     public static void add(Multiplayer mPgame3) {
         games.add(mPgame3);
+    }
+
+    public static void add(AbstractGame game) {
+        games.add(game);
+    }
+
+    public static List<AbstractGame> getGames() {
+        return games;
+    
+    }
+
+    public static void setGames(List<AbstractGame> list) {
+        return;
     }
 
 }
